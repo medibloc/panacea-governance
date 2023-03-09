bd78bc33f24286805a6701807e67509152a8d1f5
gyuguen <gyuguen.jang@medibloc.org>
2023-03-09 15:43:43 +0900

    docs: add submit script in v2.0.6 upgrade docs (#15)

---
+++ proposals/2023-03-upgrade-v2-0-6-ibc-go-upgrade/submit.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+panacead tx gov submit-proposal software-upgrade v2.0.6 \
+        --upgrade-height 9692000 \
+        --node https://rpc.gopanacea.org:443 \
+        --chain-id panacea-3 \
+        --from panacea1ewugvs354xput6xydl5cd5tvkzcuymkejekwk3 \
+        --gas auto \
+        --gas-prices 5umed \
+        --gas-adjustment 1.3 \
+        -b block \
+        --deposit "10000000000umed" \
+        --title "Panacea v2.0.6 Upgrade" \
+        --description "# Panacea v2.0.6 Upgrade
+This is a proposal to do a software upgrade to the v2.0.6 software tag of the Panacea codebase on block height 9692000, which is estimated to occur on Wednesday March 8, UTC 05:40
+
+Block times have high variance, so please monitor the chain for more precise time estimates.
+
+## Proposal
+Panacea team would like to propose that we upgrade our chain’s ibc-go from v2 to v4.3.0 (with cosmos-sdk v0.45.12 and informalsystems/tendermint v0.34.24) and remove CosmWasm from the chain.
+
+We believe that removing CosmWasm requires some explanation from our end whereas upgrading ibc-go is straightforward to most of you.
+(For more information on ibc-go upgrade, please refer to this link - https://github.com/cosmos/ibc-go/blob/main/RELEASES.md#stable-release-policy)
+
+Pancaea team first adopted CosmWasm to accomodate a possible NFT usage on the chain. However, because of below two reasons, we now think that it is more reasonable for us to remove CosmWasm from the chain.
+- No clear use case for CosmWasm at this point
+  - We also expect hardship in removing CosmWasm once somebody starts using it; currently, nobody is using CosmWasm on Panacea yet
+- Replacable through Cosmos SDK v0.46.x that now includes x/nft module later
+
+## Upgrade features
+This upgrade contains the following changes:
+- [\#635](https://github.com/medibloc/panacea-core/pull/635) Upgrade ibc-go from v2 to v4.3.0 (with cosmos-sdk v0.45.12 and informalsystems/tendermint v0.34.24)
+- [\#640](https://github.com/medibloc/panacea-core/pull/640) Remove CosmWasm
+
+## Actions required of node operators
+
+If this SoftwareUpgrade proposal is approved, the state machine of the panacead will be stopped as soon as the chain reaches to the block height 9692000.
+It means that new blocks will not be produced until the panacead daemon is restarted with the new version.
+
+Then, please replace the old panacead binary with the new one, and restart the daemon by following the guide below.
+https://github.com/medibloc/panacea-mainnet/blob/master/panacea-3/v2.0.6/upgrade.md
+
+If you are using the Cosmovisor, please build the new panacead binary manually and put that under the upgrade directory. The auto-download is not supported yet as per the following guide from cosmovisor.(https://docs.cosmos.network/main/tooling/cosmovisor#auto-download).
+
+
+## Details of Upgrade Time
+
+The proposal targets the upgrade proposal block to be 9692000, estimated to occur on Wednesday March 8, UTC 05:40. Note that block times have high variance, so keep monitoring the time. See countdown here: https://www.mintscan.io/medibloc/blocks/9692000.
+
+The upgrade is anticipated to take approx 30 minutes, during which time, there will not be any on-chain activity on the network.
+
+In the event of an issue at upgrade time, we should coordinate via the validators channel in discord to come to a quick emergency consensus and mitigate any further issues."
